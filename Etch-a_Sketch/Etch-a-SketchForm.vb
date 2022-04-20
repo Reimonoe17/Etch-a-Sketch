99a61781f57e6640e49710f83f02351e495b1f4a
Jamison Burton <jamison@burtontalk.com>
2022-04-19 19:01:19 -0600

    Add project files.

---
+++ Etch-a_Sketch/Etch-a-SketchForm.vb
@@ -0,0 +1,10 @@
+﻿'Jamison Burton
+'RCET0265
+'Spring 2022
+'Math Contest
+'https://github.com/Reimonoe17/Etch-a-Sketch
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
